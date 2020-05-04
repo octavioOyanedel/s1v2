3c9db119f2c44d75bc058152949ba0a20bafba86
Octavio Oyanedel <octavio.oyanedel@gmail.com>
2020-05-04 05:27:35 +0000

    Finalizado active nav hover, en proceso crear componente tabla, listar usuarios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 </head>
 <body>
     <div id="app">
-{{ Request()->path() }}
+<small>{{ Request()->path() }}</small>
         @if (Auth::user() != null)
             <!-- Navbar -->
             <x-nav-bar />
