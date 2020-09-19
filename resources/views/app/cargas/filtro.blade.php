0629867457f2d892635e2beeff8d24a6f05d1c85
octavio Oyanedel <octavio.oyanedel@gmail.com>
2020-09-19 00:39:56 -0300

    Corregido problema carga form filtros

--- resources/views/app/cargas/filtro.blade.php
+++ resources/views/app/cargas/filtro.blade.php
@@ -4,7 +4,7 @@
 
     <!-- formulario filtrar carga familiar -->
     <div class="contenedor-form">
-        <x-form id="filtrar-carga-form" :colecciones="$colecciones" objetos="" alinear="text-center" metodo="GET" action="filtrar_cargas" csrf="get" titulo="Búsqueda Filtrada Carga Familiar" colorBoton="btn-primary" tituloBoton="Buscar" tamanoBoton="" largoBoton="btn-block" idBoton="filtrar-carga"/>
+        <x-form id="filtrar-carga-form" :colecciones="$colecciones" objetos="" alinear="text-center" metodo="GET" action="filtrar_cargas" csrf="get" titulo="Búsqueda Filtrada Carga Familiar" colorBoton="btn-primary" tituloBoton="Buscar" tamanoBoton="" largoBoton="btn-block" idBoton="filtrar-carga" extra=""/>
     </div>
  
 @endsection
--- resources/views/app/socios/filtro.blade.php
+++ resources/views/app/socios/filtro.blade.php
@@ -4,7 +4,7 @@
 
     <!-- formulario filtrar socio -->
     <div class="contenedor-form">
-        <x-form id="filtrar-socio-form" :colecciones="$colecciones" objetos="" alinear="text-center" metodo="GET" action="filtrar_socios" csrf="get" titulo="Búsqueda Filtrada Socio" colorBoton="btn-primary" tituloBoton="Buscar" tamanoBoton="" largoBoton="btn-block" idBoton="filtrar-socio"/>
+        <x-form id="filtrar-socio-form" :colecciones="$colecciones" objetos="" alinear="text-center" metodo="GET" action="filtrar_socios" csrf="get" titulo="Búsqueda Filtrada Socio" colorBoton="btn-primary" tituloBoton="Buscar" tamanoBoton="" largoBoton="btn-block" idBoton="filtrar-socio" extra=""/>
     </div>
  
 @endsection
